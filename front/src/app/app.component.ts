59aad2b004b73d7fa1b2420abce1931360b1a803
MickC0 <krysnn@gmail.com>
2025-05-03 16:31:39 +0200

    finish home page

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,27 +1,14 @@
 import { Component } from '@angular/core';
 import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
-import {HeaderComponent} from './pages/header/header.component';
-import {NgIf} from '@angular/common';
 import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NgIf],
+  imports: [RouterOutlet, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'MDD';
-  /** URL courante, mise à jour à chaque navigation */
-  currentUrl = '';
-
-  constructor(private router: Router) {
-    // On écoute les événements de fin de navigation pour récupérer l'URL
-    this.router.events
-      .pipe(filter(e => e instanceof NavigationEnd))
-      .subscribe((e: NavigationEnd) => {
-        this.currentUrl = e.urlAfterRedirects;
-      });
-  }
 }
--- front/src/app/pages/login/login.component.ts
+++ front/src/app/pages/login/login.component.ts
@@ -1,45 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NgOptimizedImage } from '@angular/common';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatIconModule } from '@angular/material/icon';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {Location, NgOptimizedImage} from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule }      from '@angular/material/input';
+import { MatButtonModule }     from '@angular/material/button';
+import { MatIconModule }       from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [
-    RouterModule,
-    NgOptimizedImage,
-    ReactiveFormsModule,
-    MatIconModule,
+    FormsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    NgOptimizedImage,
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
-  standalone: true
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
-  loginForm!: FormGroup;
+export class LoginComponent {
+  hide = true;
+  credentials = { username: '', password: '' };
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private location: Location,
+    private router: Router
+  ) {}
+
+  goBack() {
+    this.location.back();
   }
 
-  ngOnInit(): void {
-    this.loginForm = this.fb.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-  }
-
-  onSubmit(): void {
-    if (this.loginForm.valid) {
-      const {username, password} = this.loginForm.value;
-      // TODO: appeler AuthService.login(username, password)
-      console.log('login', username, password);
-    }
+  onLogin() {
+    // TODO: appeler votre service d’authentification
+    console.log('Connexion avec', this.credentials);
+    // par exemple, rediriger vers l’accueil
+    this.router.navigate(['/']);
   }
 }
