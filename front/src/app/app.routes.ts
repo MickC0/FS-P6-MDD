cd924db9e825f45810bad439b97b6ed5ace98799
MickC0 <krysnn@gmail.com>
2025-05-04 21:18:41 +0200

    add me page

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {LoginComponent} from './pages/login/login.component';
 import {RegisterComponent} from './pages/register/register.component';
+import {MeComponent} from './pages/me/me.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login',   component: LoginComponent },
   { path: 'register',  component: RegisterComponent },
+  { path: 'me', component: MeComponent },
 ];
