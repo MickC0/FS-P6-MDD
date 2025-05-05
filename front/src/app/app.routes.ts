7b53144e6fbdfa79021cb04d9c63e076f8392bcf
MickC0 <krysnn@gmail.com>
2025-05-05 21:29:20 +0200

    add article page

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {HomeComponent} from './pages/home/home.component';
 import {LoginComponent} from './pages/login/login.component';
 import {RegisterComponent} from './pages/register/register.component';
 import {MeComponent} from './pages/me/me.component';
+import {ArticlepageComponent} from './pages/articlepage/articlepage.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login',   component: LoginComponent },
   { path: 'register',  component: RegisterComponent },
   { path: 'me', component: MeComponent },
+  { path: 'articles', component: ArticlepageComponent },
 ];
